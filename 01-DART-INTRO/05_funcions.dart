cc7334c000f5df0751715c7b96f69182ad79532c
German Pequera <pequerag@gmail.com>
2023-05-22 22:46:00 -0300

    functions

--- 01-DART-INTRO/05_funcions.dart
+++ 01-DART-INTRO/05_funcions.dart
@@ -1,8 +1,8 @@
 void main(){
   
   print(greetEveryone());
-  
   print('Suma: ${ addTwoNumbers(10, 20)}');
+  print(greetPerson(name: 'German',message: 'Hi'));
 }
 
 
@@ -19,7 +19,10 @@ String greetEveryone() => 'Hello everyone';
 int addTwoNumbers(int a, int b) => a + b;
 
 int addTwoNumbersOptional(int a,[int b = 0] ){
-  
   //b ??= 0;
   return a + b;
+}
+
+String greetPerson({required String name ,String message= 'Hola' }){
+  return '$message, $name';
 }
