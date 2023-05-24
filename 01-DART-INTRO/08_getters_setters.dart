9d731a701ed51e2e687288f49ac32a630a4d9d5d
German Pequera <pequerag@gmail.com>
2023-05-24 16:33:27 -0300

    assertions

--- 01-DART-INTRO/08_getters_setters.dart
+++ 01-DART-INTRO/08_getters_setters.dart
@@ -1,6 +1,6 @@
 void main(){
   
-  final mySquare = Square(side: 10);
+  final mySquare = Square(side: -10);
   mySquare.side = 5;
   
   
@@ -13,7 +13,8 @@ class Square{
   double _side; 
   
   Square({ required double side})
-    : _side = side;
+    : assert(side >= 0, 'side must be >= 0'),
+      _side = side;
   
   double get area{
     return _side * _side;
