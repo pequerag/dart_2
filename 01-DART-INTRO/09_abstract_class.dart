64d22c92fa77519618750c0395cf75de94a69621
German Pequera <pequerag@gmail.com>
2023-05-25 14:33:46 -0300

    abstract class extends

--- 01-DART-INTRO/09_abstract_class.dart
+++ 01-DART-INTRO/09_abstract_class.dart
@@ -1,6 +1,18 @@
 void main(){
   
+  final windPlant = WindPlant(initialEnergy: 100);
   
+  print('wind: ${ chargePhone(windPlant)}');
+  
+}
+
+
+double chargePhone( EnergyPlant plant){
+  if( plant.energyLeft < 10 ){
+    throw Exception('Not enogh energy');
+  }
+  
+  return plant.energyLeft - 10; 
   
 }
 
@@ -18,4 +30,17 @@ abstract class EnergyPlant {
     
   void consumeEnergy( double amount );
   
+}
+
+//extends o implements
+class WindPlant extends EnergyPlant { 
+  
+  WindPlant({ required double initialEnergy })
+    : super(energyLeft: initialEnergy,type: PlantType.wind);
+  
+  @override
+  void consumeEnergy( double amount ){
+    energyLeft = amount;
+  }
+    
 }
