94cc1eedf90bfca2a39d7fa03b4454ff82b97241
German Pequera <pequerag@gmail.com>
2023-05-29 14:46:55 -0300

    .

---
+++ 01-DART-INTRO/01_hello_wordl.dart
@@ -0,0 +1,12 @@
+void main(){
+  
+  //var myName = 'German';
+  //late final myName = 'German';
+  //String myName = 'German';
+  const myName = 'German';
+  
+  print('Hola $myName');
+  print('Hola ${myName.toUpperCase()}');
+  print('Hola ${ 1 + 1 }');
+  
+}
