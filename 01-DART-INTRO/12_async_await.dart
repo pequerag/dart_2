ae6afa232627699f0698c864f5c011380eea73d3
German Pequera <pequerag@gmail.com>
2023-05-29 14:46:19 -0300

    async_await

---
+++ 01-DART-INTRO/12_async_await.dart
@@ -0,0 +1,24 @@
+void main() async {
+  
+  print('Inicio del programa');
+  
+  try{
+    final value = await httpGet('dd');
+    print(value);  
+  }catch(err){
+    print('Tenemos un error: $err');
+  }
+  
+  print('Fin del programa');  
+}
+
+
+Future<String> httpGet(String url) async {
+  
+  await Future.delayed( const Duration(seconds: 1));
+  
+  throw 'Error';
+  
+//  return 'Tenemos un valor de la peticion';
+
+}
